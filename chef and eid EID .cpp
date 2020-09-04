f60d3da25e8c401f9e03cb62d47da37aeab0a758
gorakh gupta <gorakh.btech.cs19@iiitranchi.ac.in>
2020-09-04 09:09:08 +0530

    Chef and eid EID codechef problem

---
+++ chef and eid EID .cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	       int n;
+	       cin>>n;
+	       int a[n],b[n];
+	       for(int i=0;i<n;i++)
+	       {
+	              cin>>a[i];
+	       }
+	       sort(a,a+n);
+	       for(int i=0;i<n;i++)
+	       {
+	              b[i]=abs(a[i]-a[(i+1)%n]);
+	       }
+	       sort(b,b+n);
+	       cout<<b[0]<<endl;
+	}
+	return 0;
+}
